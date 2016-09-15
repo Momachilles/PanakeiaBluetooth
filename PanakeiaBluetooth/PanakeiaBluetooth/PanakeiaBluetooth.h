358fc35457e31e0cceab0a5caac74fd9d00fdf7d
David Alarcon <momachilles@gmail.com>
2016-09-15 18:58:16 +0200

    First Commit

---
+++ PanakeiaBluetooth/PanakeiaBluetooth/PanakeiaBluetooth.h
@@ -0,0 +1,19 @@
+//
+//  PanakeiaBluetooth.h
+//  PanakeiaBluetooth
+//
+//  Created by David Alarcon on 15/09/16.
+//  Copyright © 2016 Panakeia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PanakeiaBluetooth.
+FOUNDATION_EXPORT double PanakeiaBluetoothVersionNumber;
+
+//! Project version string for PanakeiaBluetooth.
+FOUNDATION_EXPORT const unsigned char PanakeiaBluetoothVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PanakeiaBluetooth/PublicHeader.h>
+
+
